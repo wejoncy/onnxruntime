67e5850c1e83dafb4187084e22b17aa4e6a2fd8b
Changming Sun <chasun@microsoft.com>
2022-09-11 14:01:14 -0700

    update

--- tools/ci_build/github/linux/build_linux_arm64_python_package.sh
+++ tools/ci_build/github/linux/build_linux_arm64_python_package.sh
@@ -1,8 +1,15 @@
 #/bin/bash
 set -x
 mkdir /build/dist
-export CFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -fcf-protection -O3 -Wl,--strip-all" 
-export CXXFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -fcf-protection -O3 -Wl,--strip-all"
+CFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -O3 -Wl,--strip-all" 
+CXXFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -O3 -Wl,--strip-all"
+arch=$(uname -i)
+if [[ $arch == x86_64* ]]; then
+    CFLAGS="$CFLAGS -fcf-protection"
+	CXXFLAGS="$CFLAGS -fcf-protection"
+fi
+export CFLAGS
+export CXXFLAGS
 PYTHON_EXES=("/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10")
 for PYTHON_EXE in "${PYTHON_EXES[@]}"
 do
