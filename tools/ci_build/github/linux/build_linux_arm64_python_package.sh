f2b15f12cae52c489d518728d914b35493b27d34
Changming Sun <chasun@microsoft.com>
2022-09-11 13:38:00 -0700

    update

--- tools/ci_build/github/linux/build_linux_arm64_python_package.sh
+++ tools/ci_build/github/linux/build_linux_arm64_python_package.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 set -x
 mkdir /build/dist
+export CFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -fcf-protection -O3 -Wl,--strip-all" 
+export CXXFLAGS="-Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -fcf-protection -O3 -Wl,--strip-all"
 PYTHON_EXES=("/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10")
 for PYTHON_EXE in "${PYTHON_EXES[@]}"
 do
