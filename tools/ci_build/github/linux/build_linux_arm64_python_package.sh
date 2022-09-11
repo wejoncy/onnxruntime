f19b3ffe306246132652eb9e3522c2adfffb6bf2
Changming Sun <chasun@microsoft.com>
2022-09-11 13:35:52 -0700

    update

--- tools/ci_build/github/linux/build_linux_arm64_python_package.sh
+++ tools/ci_build/github/linux/build_linux_arm64_python_package.sh
@@ -4,6 +4,7 @@ mkdir /build/dist
 PYTHON_EXES=("/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10")
 for PYTHON_EXE in "${PYTHON_EXES[@]}"
 do
+  rm -rf /build/Release
   ${PYTHON_EXE} /onnxruntime_src/tools/ci_build/build.py \
                   --build_dir /build \
                   --config Release --update --build \
@@ -13,5 +14,4 @@ do
                   --build_wheel \
                   --enable_onnx_tests
   cp /build/Release/dist/*.whl /build/dist
-  rm -rf /build/Release
 done
