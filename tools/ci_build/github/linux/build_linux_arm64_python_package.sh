2579265d4c5b2f55bc94fa68ef711d0437412138
Changming Sun <chasun@microsoft.com>
2022-09-12 17:49:48 -0700

    update

--- tools/ci_build/github/linux/build_linux_arm64_python_package.sh
+++ tools/ci_build/github/linux/build_linux_arm64_python_package.sh
@@ -10,7 +10,8 @@ if [[ $arch == x86_64* ]]; then
 fi
 export CFLAGS
 export CXXFLAGS
-PYTHON_EXES=("/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10")
+#PYTHON_EXES=("/opt/python/cp37-cp37m/bin/python3.7" "/opt/python/cp38-cp38/bin/python3.8" "/opt/python/cp39-cp39/bin/python3.9" "/opt/python/cp310-cp310/bin/python3.10")
+PYTHON_EXES=("/opt/python/cp38-cp38/bin/python3.8")
 for PYTHON_EXE in "${PYTHON_EXES[@]}"
 do
   rm -rf /build/Release
