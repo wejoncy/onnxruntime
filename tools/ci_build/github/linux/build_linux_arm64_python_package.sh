0c617716206664d6d970f59840fb54cd4f323f60
Changming Sun <chasun@microsoft.com>
2022-09-12 17:50:38 -0700

    update

--- tools/ci_build/github/linux/build_linux_arm64_python_package.sh
+++ tools/ci_build/github/linux/build_linux_arm64_python_package.sh
@@ -15,12 +15,12 @@ PYTHON_EXES=("/opt/python/cp38-cp38/bin/python3.8")
 for PYTHON_EXE in "${PYTHON_EXES[@]}"
 do
   rm -rf /build/Release
+  #                  --enable_lto 
   ${PYTHON_EXE} /onnxruntime_src/tools/ci_build/build.py \
                   --build_dir /build \
                   --config Release --update --build \
                   --skip_submodule_sync \
                   --parallel \
-                  --enable_lto \
                   --build_wheel \
                   --enable_onnx_tests
   cp /build/Release/dist/*.whl /build/dist
