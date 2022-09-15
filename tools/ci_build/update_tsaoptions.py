a943b4c47e23fc268ae62d9cc5db47a3ad33672a
Changming Sun <chasun@microsoft.com>
2022-09-15 11:59:46 -0700

    update

---
+++ tools/ci_build/update_tsaoptions.py
@@ -0,0 +1,18 @@
+import json
+import os
+from pathlib import Path
+
+
+SCRIPT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
+REPO_DIR = SCRIPT_DIR.parent.parent
+
+with (REPO_DIR / ".config" / "tsaoptions.json").open() as f:
+    data = json.load(f)
+
+buildNumber = os.getenv("BUILD_BUILDNUMBER")
+if buildNumber is not None:
+    data["buildNumber"] = buildNumber
+
+
+with (REPO_DIR / ".config" / "tsaoptions.json").open() as f:
+    json.dumps(data, f)
